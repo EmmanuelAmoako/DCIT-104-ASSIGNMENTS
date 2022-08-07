845c1fc89d2f651f8847d2d28286a3e9cc979ca4
EmmanuelAmoako <102744895+EmmanuelAmoako@users.noreply.github.com>
2022-08-07 21:53:03 +0000

    initial commit

---
+++ C++_even_numbers.cpp
@@ -0,0 +1,22 @@
+/*A simple code to calculate the sum of all even numbers less than a number*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int number;
+    int b = 1;
+    int sums = 0;
+
+    cout << "enter a number: ";
+    cin >> number;
+
+    while (b < number){
+        if (b%2 == 1){
+            sums = sums + b;
+        }
+    b= b+1;
+    }
+
+    cout <<"The sum of odd numbers less than " <<number<< " is " <<sums;
+}
